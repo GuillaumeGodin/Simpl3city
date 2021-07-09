be499449e6fd1cd807fb3fcc41ef97a3b7575eb1
GuillaumeGodin <godin170@hotmail.com>
2021-07-08 21:01:48 -0400

    Simpl3city

---
+++ frontend/src/products.js
@@ -0,0 +1,37 @@
+const products = [
+    {
+        _id: '1',
+        name: 'Drums Sample Pack',
+        image: '/Pictures/Drums.jpg',
+        description: '13 Kicks, 5 Top Kicks, 7 Snares, 4 Claps, 8 Toms & Congas, 8 Open & Closed Hats, 6 Crash & Rides and 9 Miscellaneous',
+        category: 'Samples',
+        price: 19.99,
+        countInStock: 0,
+        rating: 5,
+        numReviews: 4,
+    },
+    {
+        _id: '2',
+        name: 'Acoustic Guitar Loop',
+        image: '/Pictures/AcousticGuitar.jpg',
+        description: 'The acoustic guitar loop sample packs for EDM in 2021',
+        category: 'Samples',
+        price: 19.99,
+        countInStock: 3,
+        rating: 4,
+        numReviews: 4,
+    },
+    {
+        _id: '3',
+        name: 'Electric Guitar Loop',
+        image: '/Pictures/ElectricGuitar.jpg',
+        description: 'The best electric guitar loop sample packs for EDM in 2021',
+        category: 'Samples',
+        price: 19.99,
+        countInStock: 3,
+        rating: 2.5,
+        numReviews: 4,
+    },
+]
+
+export default products
